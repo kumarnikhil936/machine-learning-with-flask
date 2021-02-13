7fa4868e5cdada0c156b08a784c26744bd55a3de
Nikhil Kumar Jha <kumar.nikhil936@gmail.com>
2021-02-13 12:49:27 +0100

    YouTube comment spam detector

---
+++ YouTubeSpamDetector/app.py
@@ -0,0 +1,77 @@
+from flask import Flask, render_template, url_for, request
+import pandas as pd
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+import os.path
+
+app = Flask(__name__)
+saved_model = 'trained_model.sav'
+saved_countvec = 'count_vectorizer.sav'
+
+
+def train_model():
+    # check if a trained model is already present in the folder
+    if os.path.isfile(saved_model) & os.path.isfile(saved_countvec):
+        print('No training required! Trained model and count vectorizer already present in the folder.')
+    else:
+        # combine all the files together
+        df = pd.DataFrame()
+        for filename in ['Eminem.csv', 'KatyPerry.csv', 'LMFAO.csv', 'Psy.csv', 'Shakira.csv']:
+            df_temp = pd.read_csv(f'data/{filename}')
+            df = df.append(df_temp)
+        df = df[['CONTENT', 'CLASS']]
+        print(df.shape)
+
+        # define features and labels
+        df_x = df['CONTENT']
+        df_y = df['CLASS']
+
+        # extract features with CountVectorizer
+        corpus = df_x
+        cvec = CountVectorizer()
+        trans_x = cvec.fit_transform(corpus)
+
+        # split train and test data
+        train_x, test_x, train_y, test_y = train_test_split(trans_x, df_y, test_size=0.2, random_state=42)
+
+        # define, train, and evaluate classifier
+        clf = MultinomialNB()
+        clf.fit(train_x, train_y)
+        print(clf.score(test_x, test_y))
+
+        # save model and count_vectorizer
+        pickle.dump(clf, open(saved_model, 'wb'))
+        pickle.dump(cvec, open(saved_countvec, 'wb'))
+
+
+@app.route('/')
+def home():
+    # return 'Hello World'
+    return render_template('home.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        train_model()
+
+        # load model and count_vectorizer from the pickled files
+        clf = pickle.load(open(saved_model, 'rb'))
+        cvec = pickle.load(open(saved_countvec, 'rb'))
+
+        # get data from request
+        comment = request.form['comment']
+        data = [comment]
+
+        # make predictions
+        vect = cvec.transform(data).toarray()
+        pred = clf.predict(vect)
+
+    return render_template('result.html', prediction=pred)
+
+
+if __name__ == '__main__':
+    train_model()
+    app.run(debug=True)
---
+++ YouTubeSpamDetector/data/README.md
@@ -0,0 +1,33 @@
+### Abstract:
+It is a public set of comments collected for spam research. It has five datasets composed by 1,956 real messages extracted from five videos that were among the 10 most viewed on the collection period.
+
+###Source:
+This corpus has been collected using the YouTube Data API v3.
+
+
+### Data Set Information:
+The table below lists the datasets, the YouTube video ID, the amount of samples in each class and the total number of samples per dataset.
+
+Dataset --- YouTube ID -- # Spam - # Ham - Total \
+Psy ------- 9bZkp7q19f0 --- 175 --- 175 --- 350 \
+KatyPerry - CevxZvSJLk8 --- 175 --- 175 --- 350 \
+LMFAO ----- KQ6zr6kCPj8 --- 236 --- 202 --- 438 \
+Eminem ---- uelHwf8o7_U --- 245 --- 203 --- 448 \
+Shakira --- pRpeEdMmmQ0 --- 174 --- 196 --- 370 \
+
+Note: the chronological order of the comments were kept.
+
+
+### Attribute Information:
+
+The collection is composed by one CSV file per dataset, where each line has the following attributes:
+
+COMMENT_ID,AUTHOR,DATE,CONTENT,TAG
+
+
+
+### Relevant Papers:
+
+- Alberto, T.C., Lochter J.V., Almeida, T.A. TubeSpam: Comment Spam Filtering on YouTube. Proceedings of the 14th IEEE International Conference on Machine Learning and Applications (ICMLA'15), 1-6, Miami, FL, USA, December, 2015.
+
+- T.A. ALMEIDA, T.P. SILVA, I. SANTOS and J.M. GOMEZ HIDALGO. Text Normalization and Semantic Indexing to Enhance Instant Messaging and SMS Spam Filtering. Knowledge-Based Systems, Elsevier, 108(2016), 25-32, 2016.
